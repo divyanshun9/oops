dbb11cbddb51e35b7a7167637ddce422960cebc6
divyanshun9 <90817183+divyanshun9@users.noreply.github.com>
2021-10-06 09:41:23 +0530

    Create program.cpp

---
+++ practical06/task1/program.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<string>
+using namespace std;
+int  main()
+{
+int a=10,b=20;
+string str1= "hello",str2="world";
+int x= a+b;
+int y= str1 +a;
+string str = str1+str2;
+string s= str1+a;
+cout<<x<<" "<<y <<" "<<str<<" "<<s;
+}
