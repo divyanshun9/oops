5e984848db4f372eb7f35815b931e459890e854f
divyanshun9 <90817183+divyanshun9@users.noreply.github.com>
2021-10-30 17:45:32 +0530

    Create task3a.cpp

---
+++ practical-8/task3a.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{ 
+    cout<<info;
+    int arr[] = {10,20,30};
+ 	cout << *arr << endl;
+ 	arr++;
+ 	cout << *arr << endl;
+    return 0;
+}
